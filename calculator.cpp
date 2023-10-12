307e797990b4f72904aa4419236a29a7730565bb
Moustafa Wassim <moustafawassimabdelaziz@gmail.com>
2023-10-12 07:44:09 +0300

    c++ code

--- calculator.cpp
+++ calculator.cpp
@@ -1,84 +1,102 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
-#include <string>
-#include <algorithm>
 #include <cmath>
 #include <cstdlib>
 #include <ctime>
-#include<bits/stdc++.h>
 using namespace std ;
-    char continue_using_calc ;
-    char op ;
-    double operand1 ;
-    double operand2 ;
-    double result ;
+
+void user_enters_operand_1 (double &operand_1) ;
+void user_enters_operand_2 (double &operand_2) ;
+void user_chooses_op (char &op) ;
+void calculator_operation (double operand_1 , double operand_2 , char op) ;
+void user_wants_to_retry (char &retry) ;
 
 int main () {
+    char op ;
+    double operand_1 ;
+    double operand_2 ;
+    char retry ;
 
     while (1) {
         cout << "Welcome to our calculator!\n" ;
-        cout << "Please enter operator you want (+ or - or * or /) : " ;
-        cin >> op ;
-        while (op != '+' && op != '-' && op != '*' && op != '/') { 
-            cout << "please enter a valid operator!\n" ;
-            cout << "Please enter operator you want (+ or - or * or /) : " ;
-            cin.clear () ;
-            while (cin.get() != '\n') ;
-            cin >> op ;
-        }
-        cin.clear () ;
-        while (cin.get() != '\n') ;
-        cout << "Please enter operand 1 value : " ;
-        while (!(cin >> operand1)) {
-            cout << "please enter a valid number!\n";
-            cout << "Please enter operand 1 value : " ;
-            cin.clear () ;
-            while (cin.get () != '\n') ;
-        }
-        cout << "Please enter operand 2 value : " ;
-        while (!(cin >> operand2)) {
-            cout << "please enter a valid number!\n";
-            cout << "Please enter operand 2 value : " ;
-            cin.clear () ;
-            while (cin.get () != '\n') ;
-        }
-        switch (op) {
-            case '+' :
-                result = operand1 + operand2 ;
-                break ;
-            case '-' :
-                result = operand1 - operand2 ;
-                break ;
-            case '*' :
-                result = operand1 * operand2 ;
-                break ;
-
-            case '/' :
-                result = operand1 / operand2 ;
-                break ;
-            default :
-                cout <<"invalid op\n";
-        }
-
-        cout << operand1 << " " << op << " " << operand2 << " = " << result << '\n' ;
-
-        cout << "Would you like to continue (Y/N) ? ";
-        cin >> continue_using_calc ;
-        continue_using_calc = toupper (continue_using_calc) ;
-        while (continue_using_calc != 'Y' && continue_using_calc != 'N') {
-            cout << "Please enter a valid answer!\n";
-            cout << "Would you like to continue (Y/N) ? ";
-            cin.clear () ;
-            while (cin.get() != '\n') ;
-            cin >> continue_using_calc ;
-            continue_using_calc = toupper (continue_using_calc) ;
-        }
-        if (continue_using_calc == 'N') {
+        user_enters_operand_1 (operand_1);
+        user_enters_operand_2 (operand_2);
+        user_chooses_op (op) ;
+        calculator_operation (operand_1,operand_2,op) ;
+        user_wants_to_retry (retry) ;
+        if (toupper(retry) == 'N') {
             break ;
         }
-    } //// end of calculator program
-    cout << "Thank you of using our calc program!\n";
+    }
+
+    cout << "Thank you for your cooperation!\n" ;
+    cout << "Goodbye!\n" ;
+
     return 0;
 }
+
+void user_enters_operand_1 (double &operand_1)  {
+    cout << "Please enter operand 1 : " ;
+    while (!(cin >> operand_1)) {
+        cout << "please enter a valid number!\n" ;
+        cin.clear () ;
+        while (cin.get () != '\n' );
+        cout << "Please enter operand 1 : " ;
+    }
+
+}
+void user_enters_operand_2 (double &operand_2) {
+    cout << "Please enter operand 2 : " ;
+    while (!(cin >> operand_2)) {
+        cout << "please enter a valid number!\n" ;
+        cin.clear () ;
+        while (cin.get () != '\n' );
+        cout << "Please enter operand 2 : " ;
+    }    
+
+}
+void user_chooses_op (char &op) {
+    cout << "please choose op (+,-,*,/) : " ;
+    cin >> op ;
+    while (op != '+' && op != '-' && op != '*' && op != '/') {
+        cout << "please enter a valid op!\n" ;
+        cin.clear () ;
+        while (cin.get () != '\n' );
+        cout << "please choose op (+,-,*,/) : " ;
+        cin >> op ;
+    }
+}
+
+void calculator_operation (double operand_1 , double operand_2 , char op) {
+
+    switch (op) {
+        case '+' :
+            cout << operand_1 << " " << op << " " << operand_2 << " = " << operand_1 + operand_2 << '\n' ;
+            break ;
+        case '-' :
+            cout << operand_1 << " " << op << " " << operand_2 << " = " << operand_1 - operand_2 << '\n' ;
+            break ;
+        case '*' :
+            cout << operand_1 << " " << op << " " << operand_2 << " = " << operand_1 * operand_2 << '\n' ;
+            break ;    
+        case '/' :
+            cout << operand_1 << " " << op << " " << operand_2 << " = " << operand_1 / operand_2 << '\n' ;
+            break ;
+        default :
+            cout << "invalid operation!\n" ;
+    }
+}
+
+void user_wants_to_retry (char &retry) {
+    cout << "would you like to make another operation (Y/N) ? " ;        
+    cin >> retry ;
+    while (toupper(retry) != 'Y' && toupper(retry) != 'N') {
+        cout << "please enter a valid answer!\n";
+        cin.clear () ;
+        while (cin.get() != '\n') ;
+        cout << "would you like to make another operation (Y/N) ? " ;
+        cin >> retry ;   
+    }
     
+
+}
